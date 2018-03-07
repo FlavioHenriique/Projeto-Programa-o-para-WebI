8d23657b99d52270d1b2e4eda4a15cca54f39048
FlavioHenriique <flaviohenrique638@gmail.com>
2018-03-07 00:00:31 -0300

    Alterando command de Login, abrindo a página inicial e adicionando o usuário autenticado na sessão

--- SisRide/src/main/java/com/ifpb/sisride/command/Login.java
+++ SisRide/src/main/java/com/ifpb/sisride/command/Login.java
@@ -2,6 +2,7 @@ package com.ifpb.sisride.command;
 
 import com.ifpb.sisride.controle.GerenciadorUsuario;
 import com.ifpb.sisride.exception.AutenticacaoException;
+import com.ifpb.sisride.modelo.Usuario;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -11,6 +12,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Login implements Command {
 
@@ -25,7 +27,14 @@ public class Login implements Command {
           
             GerenciadorUsuario g = new GerenciadorUsuario();
             if(g.autenticar(request.getParameter("email"), request.getParameter("senha"))){
-              response.sendRedirect("inicial.jsp");
+              
+                HttpSession session = request.getSession();
+                
+                Usuario atual = g.buscaUsuario(request.getParameter("email"));
+                
+                session.setAttribute("usuario", atual);
+                
+                response.sendRedirect("inicial.jsp");
             }
             else{
                 response.sendRedirect("index.html");
